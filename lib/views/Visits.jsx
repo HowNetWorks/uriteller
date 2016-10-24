948d5f598207e82ed0bf9b192729941293b3e023
Joachim Viide <jviide@iki.fi>
2016-10-25 01:11:46 +0300

    Use regional indicator symbols for encoding country flags
    See https://en.wikipedia.org/wiki/Regional_Indicator_Symbol.

--- lib/views/Visits.jsx
+++ lib/views/Visits.jsx
@@ -1,22 +1,42 @@
 import React from "react";
-import emojiFlags from "emoji-flags";
 import classNames from "classnames";
 import { formatAbsolute, formatRelative } from "../timestamp";
 
-function CountryFlag(_props) {
-    const { country, ...props } = _props;
-    if (!country) {
-        return null;
+function countryFlag(code) {
+    // Turn two-letter ISO 3166-1 alpha-2 country codes to country flag emojis.
+    // See https://en.wikipedia.org/wiki/Regional_Indicator_Symbol
+
+    if (!code || code.length !== 2) {
+        return "";
     }
 
-    const info = emojiFlags.countryCode(country);
-    if (!info) {
+    const up = code.toUpperCase();
+    const a = up.charCodeAt(0) - 0x41;
+    const b = up.charCodeAt(1) - 0x41;
+    if (a < 0 || a >= 26 || b < 0 || b >= 26) {
+        return "";
+    }
+
+    const base = 0x1f1e6;
+    return String.fromCodePoint(base + a, base + b);
+}
+
+function CountryFlag(_props) {
+    const { country, ...props } = _props;
+
+    const flag = countryFlag(country);
+    if (!flag) {
         return null;
     }
 
     const className = classNames(props.className, "country-flag");
-    return <span className={className} {...props}>{info.emoji}</span>;
+    return <span className={className} {...props}>{flag}</span>;
 }
+
+CountryFlag.propTypes = {
+    country: React.PropTypes.string,
+    className: React.PropTypes.string,
+};
 
 function copy(text) {
     const div = document.createElement("div");
