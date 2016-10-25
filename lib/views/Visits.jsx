54248788f9a08a09da5ddc4d53a077367ba52f48
Joachim Viide <jviide@iki.fi>
2016-10-26 00:32:35 +0300

    Add an explanatory text

--- lib/views/Visits.jsx
+++ lib/views/Visits.jsx
@@ -28,14 +28,58 @@ Cell.propTypes = {
     children: React.PropTypes.node
 };
 
+function NoVisits() {
+    return (
+        <div className="text-muted">No-one has visited the trap yet.</div>
+    );
+}
+
+function Table(_props) {
+    const { className, visits, js, ...props } = _props;
+    const cn = classNames("table", "table-striped", className);
+
+    return (
+        <table className={cn} {...props}>
+            <thead>
+                <tr>
+                    <th>Time</th>
+                    <th>IP</th>
+                    <th>ASN</th>
+                    <th>User Agent</th>
+                </tr>
+            </thead>
+            <tbody>
+                {visits.map((visit, index) => (
+                    <tr key={index}>
+                        <Cell className="timestamp" header="Time">
+                            <Timestamp timestamp={visit.timestamp} update={js} />
+                        </Cell>
+                        <Cell className="ip" header="IP">
+                            <CountryFlag country={visit.country} />{visit.ip}
+                        </Cell>
+                        <Cell header="ASN">
+                            {visit.asns.map((item, index) => (
+                                <span key={index}>{item.asn} {item.names}</span>
+                            ))}
+                        </Cell>
+                        <Cell header="User Agent">{visit.userAgent}</Cell>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    );
+}
+
 export default function Visits(props) {
     return (
         <div className="container">
             <section className="row">
-                <div className="col-sm-12">
-                    <h4>Tracking visits to</h4>
+                <div className="col-xs-12">
+                    <h4>Monitoring visits to URL</h4>
+                </div>
 
-                    <div className="input-group">
+                <div className="col-xs-12 col-lg-8">
+                    <div className="input-group trap-group">
                         <input id="trap-url" className="form-control trap-url" value={props.trapUrl} readOnly />
 
                         <span className="input-group-btn">
@@ -45,40 +89,20 @@ export default function Visits(props) {
                         </span>
                     </div>
                 </div>
+
+                <div className="col-xs-12 col-lg-4 text-justify help">
+                    This is your <strong>trap URL</strong>. Copy-paste it to
+                    your favorite messaging app, URL shortener or social network
+                    site. This <strong>monitor page</strong> shows who visits
+                    the trap.
+                </div>
             </section>
 
             <section className="row">
                 <div className="col-sm-12">
                     <h4>Visits</h4>
 
-                    <table className="table table-striped visits">
-                        <thead>
-                            <tr>
-                                <th>Time</th>
-                                <th>IP</th>
-                                <th>ASN</th>
-                                <th>User Agent</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {props.visits.map((visit, index) => (
-                                <tr key={index}>
-                                    <Cell className="timestamp" header="Time">
-                                        <Timestamp timestamp={visit.timestamp} update={props.js} />
-                                    </Cell>
-                                    <Cell className="ip" header="IP">
-                                        <CountryFlag country={visit.country} />{visit.ip}
-                                    </Cell>
-                                    <Cell header="ASN">
-                                        {visit.asns.map((item, index) => (
-                                            <span key={index}>{item.asn} {item.names}</span>
-                                        ))}
-                                    </Cell>
-                                    <Cell header="User Agent">{visit.userAgent}</Cell>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
+                    {props.visits.length === 0 ? <NoVisits /> : <Table className="visits" visits={props.visits} js={props.js} />}
                 </div>
             </section>
         </div>
