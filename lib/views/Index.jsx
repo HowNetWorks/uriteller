785db1dd9752ec198fc2e7d857932f39cacb68a9
Joachim Viide <jviide@iki.fi>
2016-10-25 19:56:04 +0300

    Modify the tagline

--- lib/views/Index.jsx
+++ lib/views/Index.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 export default function Index() {
     return (
-        <div className="index-header">
-            <div className="container">
-                <h1>URI:teller</h1>
-                <p className="lead">Who's calling back?</p>
-                <div>
-                <a className="btn btn-primary btn-lg" href="/new">Let's find out!</a>
-                </div>
-            </div>
+        <div className="container index">
+            <h1>URI:teller</h1>
+
+            <p className="lead">
+                Are <strong>they</strong> calling back <strong>from beyond the cloud</strong>?
+            </p>
+
+            <a className="btn btn-primary btn-lg" href="/new">Let's find out!</a>
         </div>
     );
 }
