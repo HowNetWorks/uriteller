919b10ef41d3ae9a46cbc663e6f31236a4e5b631
Joachim Viide <jviide@iki.fi>
2016-10-26 17:15:13 +0300

    Separate modules

--- lib/components/CopyButton.jsx
+++ lib/components/CopyButton.jsx
@@ -1,38 +1,9 @@
 import React from "react";
-
-function copy(text) {
-    const div = document.createElement("div");
-    div.appendChild(document.createTextNode(text));
-
-    const selection = window.getSelection();
-    const oldRanges = [];
-    for (var i = 0, len = selection.rangeCount; i < len; i++) {
-        oldRanges.push(selection.getRangeAt(i));
-    }
-
-    document.body.appendChild(div);
-    try {
-        const range = document.createRange();
-        range.selectNode(div);
-
-        const selection = window.getSelection();
-        selection.removeAllRanges();
-        selection.addRange(range);
-
-        document.execCommand("copy");
-    } finally {
-        document.body.removeChild(div);
-
-        selection.removeAllRanges();
-        oldRanges.forEach(oldRange => {
-            selection.addRange(oldRange);
-        });
-    }
-}
+import copyToClipboard from "../copy-to-clipboard";
 
 export default function CopyButton(_props) {
     const { text, ...props } = _props;
-    return <button onClick={() => copy(text)} {...props} />;
+    return <button onClick={() => copyToClipboard(text)} {...props} />;
 }
 
 CopyButton.propTypes = {
--- lib/components/CountryFlag.jsx
+++ lib/components/CountryFlag.jsx
@@ -1,24 +1,6 @@
 import React from "react";
 import classNames from "classnames";
-
-function countryFlag(code) {
-    // Turn two-letter ISO 3166-1 alpha-2 country codes to country flag emojis.
-    // See https://en.wikipedia.org/wiki/Regional_Indicator_Symbol
-
-    if (!code || code.length !== 2) {
-        return "";
-    }
-
-    const up = code.toUpperCase();
-    const a = up.charCodeAt(0) - 0x41;
-    const b = up.charCodeAt(1) - 0x41;
-    if (a < 0 || a >= 26 || b < 0 || b >= 26) {
-        return "";
-    }
-
-    const base = 0x1f1e6;
-    return String.fromCodePoint(base + a, base + b);
-}
+import countryFlag from "../country-flag";
 
 export default function CountryFlag(_props) {
     const { country, ...props } = _props;
