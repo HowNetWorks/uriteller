3a5907231b4ddb0bc9864f4acea4054838dba980
Joachim Viide <jviide@iki.fi>
2017-01-17 04:42:19 +0200

    Don't use @google/cloud-errors when NODE_ENV !== "production"

--- server/lib/gcloud.js
+++ server/lib/gcloud.js
@@ -2,12 +2,23 @@ if (!process.env.GCLOUD_PROJECT) {
     process.env.GCLOUD_PROJECT = process.env.GAE_LONG_APP_ID;
 }
 
-export const errors = require("@google/cloud-errors").start();
+let errorHandler = {
+    report(err) {
+        // eslint-disable-next-line no-console
+        console.error(err);
+    },
+    express(req, res, next) {
+        next();
+    }
+};
 
 if (process.env.NODE_ENV === "production") {
     require("@google/cloud-trace").start();
     require("@google/cloud-debug").start();
+    errorHandler = require("@google/cloud-errors").start();
 }
+
+export const errors = errorHandler;
 
 export default require("google-cloud")({
     projectId: process.env.GCLOUD_PROJECT || process.env.GAE_LONG_APP_ID
