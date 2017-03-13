11f18154cc750649327afa1ee3dffb8fa94d2e8b
Joachim Viide <jviide@iki.fi>
2017-03-13 23:28:09 +0200

    Move update fetching into its own module

---
+++ src/frontend/lib/fetch-updates.js
@@ -0,0 +1,74 @@
+import url from "url";
+
+function timeout(delay) {
+  return new Promise(resolve => {
+    setTimeout(resolve, delay);
+  });
+}
+
+class ConnectionFailed extends Error {}
+class RequestFailed extends Error {}
+class ParsingFailed extends Error {}
+class Timeout extends Error {}
+
+function fetchJSON(url, timeout=15000) {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", url);
+    xhr.timeout = timeout;
+
+    xhr.addEventListener("load", () => {
+      if (xhr.status !== 200) {
+        return reject(new RequestFailed(`status ${xhr.status} (${xhr.statusText})`));
+      }
+
+      const response = xhr.response;
+      let json;
+      try {
+        json = JSON.parse(response);
+      } catch (err) {
+        return reject(new ParsingFailed("parsing the request response failed"));
+      }
+      resolve(json);
+    }, false);
+
+    xhr.addEventListener("error", () => {
+      reject(new ConnectionFailed("connection failed"));
+    }, false);
+
+    xhr.addEventListener("timeout", () => {
+      reject(new Timeout("timeout"));
+    }, false);
+
+    xhr.send();
+  });
+}
+
+function fetchLoop(baseUrl, cursor, interval, minInterval, maxInterval, currentErr, callback) {
+  const parsedUrl = url.parse(baseUrl, true);
+  parsedUrl.query.cursor = cursor;
+
+  const updateUrl = url.format(parsedUrl);
+  timeout(interval)
+    .then(() => fetchJSON(updateUrl))
+    .then(
+      json => {
+        const visits = json.visits;
+        if (currentErr || visits.length > 0) {
+          callback(null, visits);
+        }
+        fetchLoop(baseUrl, json.cursor, minInterval, minInterval, maxInterval, null, callback);
+      },
+      err => {
+        if (!currentErr || String(currentErr) !== String(err)) {
+          callback(err, null);
+        }
+        const newInterval = Math.min(interval * 2, maxInterval);
+        fetchLoop(baseUrl, cursor, newInterval, minInterval, maxInterval, err, callback);
+      }
+    );
+}
+
+export default function(baseUrl, cursor, minInterval, maxInterval, callback) {
+  fetchLoop(baseUrl, cursor, minInterval, minInterval, maxInterval, null, callback);
+}
--- src/frontend/views/Monitor.vue
+++ src/frontend/views/Monitor.vue
@@ -44,7 +44,7 @@
 </template>
 
 <script>
-import url from "url";
+import fetchUpdates from "../lib/fetch-updates";
 import VisitTable from "../components/VisitTable.vue";
 import CopyButton from "../components/CopyButton.vue";
 
@@ -55,81 +55,11 @@ function byTimestampDescending(left, right) {
   return left.timestamp < right.timestamp ? 1 : -1;
 }
 
-function timeout(delay) {
-  return new Promise(resolve => {
-    setTimeout(resolve, delay);
-  });
-}
-
-class ConnectionFailed extends Error {}
-class RequestFailed extends Error {}
-class ParsingFailed extends Error {}
-class Timeout extends Error {}
-
-function fetchJSON(url, timeout=15000) {
-  return new Promise((resolve, reject) => {
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET", url);
-    xhr.timeout = timeout;
-
-    xhr.addEventListener("load", () => {
-      if (xhr.status !== 200) {
-        return reject(new RequestFailed(`status ${xhr.status} (${xhr.statusText})`));
-      }
-
-      const response = xhr.response;
-      let json;
-      try {
-        json = JSON.parse(response);
-      } catch (err) {
-        return reject(new ParsingFailed("parsing the request response failed"));
-      }
-      resolve(json);
-    }, false);
-
-    xhr.addEventListener("error", () => {
-      reject(new ConnectionFailed("connection failed"));
-    }, false);
-
-    xhr.addEventListener("timeout", () => {
-      reject(new Timeout("timeout"));
-    }, false);
-
-    xhr.send();
-  });
-}
-
-function fetchUpdates(baseUrl, cursor, interval, minInterval, maxInterval, currentErr, callback) {
-  const parsedUrl = url.parse(baseUrl, true);
-  parsedUrl.query.cursor = cursor;
-
-  const updateUrl = url.format(parsedUrl);
-  timeout(interval)
-    .then(() => fetchJSON(updateUrl))
-    .then(
-      json => {
-        const visits = json.visits;
-        if (currentErr || visits.length > 0) {
-          callback(null, visits);
-        }
-        fetchUpdates(baseUrl, json.cursor, minInterval, minInterval, maxInterval, null, callback);
-      },
-      err => {
-        if (!currentErr || String(currentErr) !== String(err)) {
-          callback(err, null);
-        }
-        const newInterval = Math.min(interval * 2, maxInterval);
-        fetchUpdates(baseUrl, cursor, newInterval, minInterval, maxInterval, err, callback);
-      }
-    );
-}
-
 export default {
   data() {
     return {
       trapUrl: null,
       visits: [],
-
       cursor: null,
       liveUpdateError: null,
       serverSide: true
@@ -146,7 +76,7 @@ export default {
     const minInterval = 1000;
     const maxInterval = 15000;
 
-    fetchUpdates(baseUrl, cursor, minInterval, minInterval, maxInterval, null, (err, visits) => {
+    fetchUpdates(baseUrl, cursor, minInterval, maxInterval, (err, visits) => {
       if (err) {
         this.liveUpdateError = err;
       } else {
