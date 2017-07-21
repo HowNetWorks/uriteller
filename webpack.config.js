aae6fb65bf7cd0946816099e5ef4c0a33d514c15
Joachim Viide <jviide@iki.fi>
2017-07-21 16:42:53 +0300

    Cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,14 @@ function p(...paths) {
   return path.join(__dirname, ...paths);
 }
 
+const babel = {
+  loader: "babel-loader",
+  options: {
+    presets: [["env", { modules: false, targets: { uglify: true } }]],
+    plugins: ["transform-object-rest-spread"]
+  }
+};
+
 const base = {
   output: {
     path: p("build"),
@@ -24,46 +32,18 @@ const base = {
       {
         test: /\.js$/,
         include: [p("src")],
-        loader: "babel-loader",
-        options: {
-          presets: [["env", { modules: false, targets: { uglify: true } }]],
-          plugins: ["transform-object-rest-spread"]
-        }
+        use: [babel]
       },
       {
         test: /\.vue$/,
         include: [p("src")],
         loader: "vue-loader",
         options: {
+          extractCSS: true,
           loaders: {
-            js: {
-              loader: "babel-loader",
-              options: {
-                presets: [
-                  ["env", { modules: false, targets: { uglify: true } }]
-                ],
-                plugins: ["transform-object-rest-spread"]
-              }
-            }
+            js: babel
           }
         }
-      },
-      {
-        test: /\.s?css$/,
-        loader: ExtractTextPlugin.extract([
-          {
-            loader: "css-loader",
-            options: {
-              sourceMap: true
-            }
-          },
-          {
-            loader: "sass-loader",
-            options: {
-              sourceMap: true
-            }
-          }
-        ])
       },
       {
         test: /\.(eot|woff2|woff|ttf|svg|png)$/,
@@ -79,8 +59,7 @@ const base = {
       filename: "assets/[contenthash].css"
     }),
     new CompressionPlugin()
-  ],
-  devtool: "source-map"
+  ]
 };
 
 module.exports = [
@@ -90,7 +69,8 @@ module.exports = [
       new webpack.optimize.ModuleConcatenationPlugin(),
       new CleanPlugin(["build/assets"]),
       new VueSsrClientPlugin()
-    ]
+    ],
+    devtool: "source-map"
   }),
   merge(base, {
     target: "node",
